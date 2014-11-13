0e748a48c7f621e0efab2f5ce184d92a5b6d369c
Théo Zimmermann <theo.zim@free.fr>
2014-11-13 14:36:00 +0100

    reverse comments order

--- isso/db/comments.py
+++ isso/db/comments.py
@@ -128,7 +128,7 @@ class Comments:
             order_by = 'id'
         sql.append('ORDER BY ')
         sql.append(order_by)
-        sql.append(' ASC')
+        sql.append(' DESC')
 
         if limit:
             sql.append('LIMIT ?')
