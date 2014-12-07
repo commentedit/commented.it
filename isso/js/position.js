8993fc7c292180f523720fd896aeac105f4d3fa5
Théo Zimmermann <theo.zim@free.fr>
2014-12-07 20:03:25 +0100

    transform position into object constructor

--- isso/js/position.js
+++ isso/js/position.js
@@ -3,28 +3,23 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var position = function(page_element_id) {
+function position(page_element_id) {
 
     "use strict";
 
     var page = document.getElementById(page_element_id);
     var isso = document.getElementById("isso-thread");
 
-    var column = function() {
+    this.column = function() {
         page.style.margin-left = "5%";
         isso.style.position = "fixed";
         isso.style.left = "calc(" + page.offsetWidth + "px + 5%)";
     }
 
-    var basic = function() {
+    this.basic = function() {
         page.style.margin-left = "auto";
         isso.style.position = "static";
     }
-
-    return {
-        column: column,
-        basic: basic
-    };
 
 };
 
