6068123a35687a0f80c570c81e00f970b5c2fff0
Théo Zimmermann <theo.zim@free.fr>
2015-02-21 18:37:39 +0100

    update to use jQuery

--- isso/js/app/slider.js
+++ isso/js/app/slider.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-define(["app/dom"], function($) {
+define(["jquery"], function($) {
 
     "use strict";
 
@@ -21,19 +21,19 @@ define(["app/dom"], function($) {
         after = aft; // what to do after changing of current block
         // after takes the new block as argument
 
-        slider = $.htmlify('<div id="slider"></div>');
+        slider = $('<div id="slider"></div>');
         // slider position
         // there may be a better way (and we don't handle resizing for now)
-        slider.style.left =
-            ((article.getBoundingClientRect().right
-              + $("#isso-thread").getBoundingClientRect().left) / 2
-             - 10) + "px";
+        slider.css("left",
+            ((article.offset().right
+              + $("#isso-thread").offset().left) / 2
+             - 10) + "px");
         article.insertAfter(slider);
 
-        cursor = $.htmlify('<div id="cursor"></div>');
+        cursor = $('<div id="cursor"></div>');
         slider.append(cursor);
 
-        var first_block = blocks[0].getBoundingClientRect();
+        var first_block = blocks.offset();
         // this is not good if block is is higher than the window
         setTimeout(function() {
             set_cursor_position((first_block.top + first_block.bottom) / 2);
@@ -42,14 +42,14 @@ define(["app/dom"], function($) {
 
     // cursor position
     var set_cursor_position = function(y) {
-        var slider_rect = slider.getBoundingClientRect();
+        var slider_rect = slider.offset();
         var slider_height = slider_rect.bottom - slider_rect.top;
         var raw_position = y - slider_rect.top - 10;
         var corrected_position =
             (raw_position < 0) ?
             0 : ((raw_position > slider_height - 20) ?
                  slider_height - 20 : raw_position);
-        cursor.style.top = corrected_position + "px";
+        cursor.css("top", corrected_position + "px");
         // current_position is useful to determine the current block
         current_position = y + slider_rect.top + 10;
     };
@@ -57,10 +57,10 @@ define(["app/dom"], function($) {
     var update_current_block = function() {
         var i = 0;
         while (i < blocks.length &&
-            blocks[i].getBoundingClientRect().top <= current_position) {
+            blocks.eq(i).offset().top <= current_position) {
             i++;
         }
-        var new_block = blocks[i - 1];
+        var new_block = blocks.eq(i - 1);
         if (new_block !== current_block) {
             // To do before updating block
             before();
@@ -72,12 +72,8 @@ define(["app/dom"], function($) {
     };
 
     var highlight_current_block = function() {
-        for (var i = 0; i < blocks.length; i++) {
-            if (blocks[i].classList.contains("current-block")) {
-                blocks[i].classList.remove("current-block");
-            }
-        }
-        current_block.classList.add("current-block");
+        blocks.removeClass("current-block");
+        current_block.addClass("current-block");
     };
 
     return {
